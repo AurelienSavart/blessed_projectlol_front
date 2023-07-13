beac82245c3c80aa53b7e0b5fca1f0828b54fa54
MaxBlach <maxime.blachere@livecampus.tech>
2023-07-13 15:21:28 +0200

    Ajout du nom et de la photo

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,33 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [champions, setChampions] = useState<any>({});
+
+  const champions: any = {
+    "Aatrox": {
+        "id":"Aatrox",
+        "key":"266",
+        "name":"Aatrox",
+        "title":"Épée des Darkin",
+        "blurb":"Autrefois, Aatrox et ses frères étaient honorés pour avoir défendu Shurima contre le Néant. Mais ils finirent par devenir une menace plus grande encore pour Runeterra : la ruse et la sorcellerie furent employées pour les battre. Cependant, après des...",
+        "image":{
+            "full":"Aatrox.png",
+            "sprite":"champion0.png",
+            "group":"champion",
+            "x":0,
+            "y":0,
+            "w":48,
+            "h":48},
+        "tags":
+            ["Fighter","Tank"]
+          },
+          "Bard": {
+            "id":"Bard",
+            "key":"256",
+            "name":"Bard",
+            "title":"Le gros Bard",
+            "blurb":"Autrefois, Bard et ses frères étaient honorés pour avoir défendu Shurima contre le Néant. Mais ils finirent par devenir une menace plus grande encore pour Runeterra : la ruse et la sorcellerie furent employées pour les battre. Cependant, après des...",
+            "image":{
+                "full":"Bard.png",
+                "sprite":"champion0.png",
+                "group":"champion",
+                "x":0,
+                "y":0,
+                "w":48,
+                "h":48},
+            "tags":
+                ["Buster","Peugeot 208"]
+              }
+    };
+  // useEffect(() => {
+  //   function getData() {
+  //     return fetch('http://localhost:3000/champions') 
+  //       .then(response => response.json())
+  //       .then(data => {
+  //         console.log("data", data);
+  //         setChampions(data.data)
+  //       })
+  //   }
+
+  //   getData()
+  // }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="min-h-screen bg-slate-900 grid grid-cols-4 gap-4">
+        {Object.keys(champions).map((champion) => (
+        <div key={champions[champion].key} className="champion">
+          <a href={'/champion/' + champions[champion].name}>
+              <img src={`https://ddragon.leagueoflegends.com/cdn/13.13.1/img/champion/${champions[champion].image.full}`} width={200} height={200} alt={champions[champion].name} />
+              <h2 className="text-lg font-bold text-white">{champions[champion].name}</h2>
+          </a>
+        </div>
+      ))}
+    </main>
     </>
   )
 }
